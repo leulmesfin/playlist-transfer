a95b353fa72cee722803289e4bd4878ccdd3f53d
Leul Mesfin <leulmesfin152025@gmail.com>
2024-08-29 14:17:15 -0400

    can successfully transfer playlists to apple music now

--- src/routes/result.jsx
+++ src/routes/result.jsx
@@ -2,15 +2,13 @@ import React, { useEffect } from 'react'
 import useMusicKit from '../components/MusicKitHook';
 import { usePlaylist } from '../components/PlaylistProvider';
 
-
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 export const ResultRoute = () => {
     const {musicKit, instance} = useMusicKit();
     const { selectedPlaylists } = usePlaylist();
     
     useEffect(() => {
         const getPlaylists = async () => {
-            // // const result = await musicKit.api.library.playlists();
-            // // console.log("res: ", result);
             // console.log("music kit: ", musicKit);
             // console.log("instance: ", instance);
             console.log("chosen playlists in result: ", selectedPlaylists);
@@ -37,18 +35,6 @@ export const getHeader = (instance) => {
     return header
 }
 
-// let data = {
-//     "attributes": {
-//         "name": "test",
-//         "description": "..."
-//     },
-//     "relationships": {
-//         "tracks": {
-//             "data": []
-//         }
-//     }
-// }
-
 // this function makes a POST request to create a new playlist in an authorized user's Apple Music library
 const createAppleMusicPlaylist = async (instance, data) => {
     const response = await fetch('https://api.music.apple.com/v1/me/library/playlists', {
@@ -65,15 +51,15 @@ const createAppleMusicPlaylist = async (instance, data) => {
 
 // function that 1. creates a playlist for each playlist in selectedPlaylists, 2. then adds its tracks to that playlist
 // get 1 working first (done), get 2
-const uploadPlaylistsToAppleMusic = (musicKitInstance, playlists) => {
+const uploadPlaylistsToAppleMusic = async (musicKitInstance, playlists) => {
     // map thru every playlist in playlists, create a playlist
-    // enter the name, and album image
+    // map thru songs (synchronously) and add to playlist
     console.log("playlists: ", playlists);
-    playlists.map(async (playlist) => {
-        // go thru each track, push to list and use for data
+    for (const playlist of playlists) {
         const songIdList = [];
-        playlist.songs.map(async (song) => {
-            // verify if track exists in apple music before using it
+        for (const song of playlist.songs) {
+            console.log("song: ", song);
+            await delay(50); // Wait .05 second between each songId fetch
             const songId = await getSongId(musicKitInstance, song.name, song.artist)
             if (songId) {
                 const trackData = {
@@ -81,17 +67,16 @@ const uploadPlaylistsToAppleMusic = (musicKitInstance, playlists) => {
                     "type": "songs"
                 }
                 songIdList.push(trackData);
-                // const theSong = await getSong(musicKitInstance, songId)
+                //const theSong = await getSong(musicKitInstance, songId)
                 // song id for testin: 187454421
                 // console.log("track id: ", songId)
-                // console.log("the song: ", theSong)
+                //console.log("the song: ", theSong)
                 // console.log("tracklist ids: ", songIdList)
             } else {
                 console.log("OOPS! The track with name: " + song.name + " and artistName: " + song.artist + " does not exist in Apple Music :(");
             }
+        }
 
-        })
-        // create data
         let data = {
             "attributes": {
                 "name": playlist.name,
@@ -116,7 +101,31 @@ const uploadPlaylistsToAppleMusic = (musicKitInstance, playlists) => {
         }
         console.log("body before adding: ", body)
         addTracksToPlaylist(musicKitInstance, playlistId, body);
-    })
+    }
+    // playlists.map(async (playlist) => {
+    //     // go thru each track, push to list and use for data
+    //     const songIdList = [];
+    //     await Promise.all(playlist.songs.map(async (song) => {
+    //         // verify if track exists in apple music before using it
+    //         const songId = await getSongId(musicKitInstance, song.name, song.artist)
+    //         if (songId) {
+    //             const trackData = {
+    //                 "id": songId,
+    //                 "type": "songs"
+    //             }
+    //             songIdList.push(trackData);
+    //             //const theSong = await getSong(musicKitInstance, songId)
+    //             // song id for testin: 187454421
+    //             // console.log("track id: ", songId)
+    //             //console.log("the song: ", theSong)
+    //             // console.log("tracklist ids: ", songIdList)
+    //         } else {
+    //             console.log("OOPS! The track with name: " + song.name + " and artistName: " + song.artist + " does not exist in Apple Music :(");
+    //         }
+    //     }));
+        // create data
+        
+    // })
 }
 
 const getSongId = async (musicKitInstance, songName, artist) => {
@@ -160,15 +169,24 @@ const getSong = async (musicKitInstance, songId) => {
     }
 }
 
-const addTracksToPlaylist = async (musicKitInstance, playlistId, songIdList) =>  {
-    const response = await fetch(`https://api.music.apple.com/v1/me/library/playlists/${playlistId}/tracks`, {
-        headers: getHeader(musicKitInstance),
-        method: "POST",
-        body: JSON.stringify(songIdList),
-        mode: 'cors'
-    })
+const addTracksToPlaylist = async (musicKitInstance, playlistId, body) =>  {
+    // extract songId list from the body
+    const songIdList = body.data;
+    console.log("SONG ID LIST: ", songIdList)
+    for (const song of songIdList) {
+        await delay(50); // Wait 0.05 seconds between each track addition
+        const response = await fetch(`https://api.music.apple.com/v1/me/library/playlists/${playlistId}/tracks`, {
+            headers: getHeader(musicKitInstance),
+            method: "POST",
+            body: JSON.stringify({ data: [song] }),
+            mode: 'cors'
+        })
+        console.log(`Added track ${song.id}, status: ${response.status}`);
+    }
+   
 
-    const responseVal = response.text()
-    console.log("response after adding tracks: ", responseVal);
-    return response.status;
+    // const responseVal = response.text()
+    // console.log("response after adding tracks: ", responseVal);
+    // return response.status;
+    return null;
 }
